b71becee0f71ec77056491c9c8775bb3fed447c6
Kaz Delphi <2746975606@qq.com>
2019-11-10 10:55:00 +0800

    添加 玩家初始化 子弹 位移函数 ...

--- src/GameData.h
+++ src/GameData.h
@@ -1,9 +1,19 @@
 #pragma once
 #include "System.h"
+
 #define MapHeight 20 //地图长定义
 #define MapWidth 25  //地图宽定义
+typedef enum _Direct_
+{
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+} Direct;
+
 short Map[25][25];
-struct Player //玩家信息定义
+
+typedef struct _Player_
 {
     char symbol;
     short attack;
@@ -11,8 +21,24 @@ struct Player //玩家信息定义
     short health;
     short x;
     short y;
-};
-void MapPrint()
+} Player; //玩家结构体定义
+typedef struct _Bullet_
+{
+    char symbol;
+    short attack;
+    short direct;
+    short x;
+    short y;
+
+} Bullet; //子弹结构体定义
+void Down();
+void InitMap();
+void InitPlayer(Player *player, char symbol, short attack, short direct, short health, short x, short y);
+void Left();
+void Right();
+void Up();
+
+void InitMap()
 {
     for (short tmp = 0; tmp < MapWidth; tmp++)
     {
@@ -31,4 +57,14 @@ void MapPrint()
         Map[MapHeight - 1][tmp] = '*';
         LocateWrite(MapHeight - 1, tmp, '*');
     }
+}
+void InitPlayer(Player *player, char symbol, short attack, short direct, short health, short x, short y)
+{
+    player->symbol = symbol;
+    player->attack = attack;
+    player->direct = direct;
+    player->health = health;
+    player->x = x;
+    player->y = y;
+    LocateWrite(player->y, player->x, player->symbol);
 }
